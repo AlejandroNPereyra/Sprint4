f9b00108916256513745704098d7f6185c51070e
AlejandroNPereyra <pereyra_alejandro@yahoo.com.ar>
2023-10-27 18:30:17 +0200

    + Duel != Winner/Loser Selector

--- ITAcademyLeague/resources/views/duelviews/createDuel.blade.php
+++ ITAcademyLeague/resources/views/duelviews/createDuel.blade.php
@@ -29,41 +29,41 @@
                 @endforeach
             </select>
             <br>
-            @error('celebrated_at')
+            @error('celebrated_at') 
                 <span class="font-mtg-title text-red-500 text-center"> {{ $message }} </span>
             @enderror
             <br>
         </div>
 
         <div class="mb-4">
-            <label for= "winner_commander" class="block text-mtg-text font-semibold">Winner</label>
-            <select id="winner_commander" name="winner_commander" class="w-full px-4 py-2 rounded-lg bg-mtg-input border-2 border-mtg-border text-black focus:outline-none focus-border-mtg-highlight">
+            <label for="winner_commander" class="block text-mtg-text font-semibold">Winner</label>
+                <select id="winner_commander" name="winner_commander" class="w-full px-4 py-2 rounded-lg bg-mtg-input border-2 border-mtg-border text-black focus:outline-none focus-border-mtg-highlight">
                 <option value="" disabled selected>Select a commander</option>
-                @foreach($commanders as $commander)
-                    <option value="{{ $commander->commander_ID }}" {{ old('winner_commander') == $commander->commander_ID ? 'selected' : '' }}>{{ $commander->commander_name }}</option>
-                @endforeach
-            </select>
-            <br>
-            @error('winner_commander')
-                <span class="font-mtg-title text-red-500 text-center"> {{ $message }} </span>
-            @enderror
-            <br>
+            @foreach($commanders as $commander)
+                <option value="{{ $commander->commander_ID }}">{{ $commander->commander_name }}</option>
+            @endforeach
+        </select>
+                <br>
+                @error('winner_commander')
+                    <span class="text-red-500">{{ $message }}</span>
+                @enderror
+                <br>
         </div>
 
-        <div class="mb-4">
-            <label for="loser_commander" class="block text-mtg-text font-semibold">Loser</label>
-            <select id="loser_commander" name="loser_commander" class="w-full px-4 py-2 rounded-lg bg-mtg-input border-2 border-mtg-border text-black focus:outline-none focus-border-mtg-highlight">
-                <option value="" disabled selected>Select a commander</option>
-                @foreach($commanders as $commander)
-                    <option value="{{ $commander->commander_ID }}" {{ old('loser_commander') == $commander->commander_ID ? 'selected' : '' }}>{{ $commander->commander_name }}</option>
-                @endforeach
-            </select>
-            <br>
-            @error('loser_commander')
-                <span class="font-mtg-title text-red-500 text-center"> {{ $message }} </span>
-            @enderror
-            <br>
-        </div>
+            <div class="mb-4">
+                <label for="loser_commander" class="block text-mtg-text font-semibold">Loser</label>
+        <select id="loser_commander" name="loser_commander" class="w-full px-4 py-2 rounded-lg bg-mtg-input border-2 border-mtg-border text-black focus:outline-none focus-border-mtg-highlight">
+            <option value="" disabled selected>Select a commander</option>
+            @foreach($commanders as $commander)
+                <option value="{{ $commander->commander_ID }}">{{ $commander->commander_name }}</option>
+            @endforeach
+        </select>
+                <br>
+                @error('loser_commander')
+                    <span class="text-red-500">{{ $message }}</span>
+                @enderror
+                <br>
+            </div>
 
         <div class="mb-4">
             <label for="winner_mana_used" class="block text-mtg-text font-semibold">Winner's Mana Used</label>
@@ -88,5 +88,38 @@
         <input type="submit" value="Summon" class="bg-mtg-button text-mtg-button-text px-4 py-2 rounded-lg transition duration-300 transform hover:scale-105 focus:outline-none">
     </form>
 </div><br><br>
+
+<script>
+
+    document.addEventListener("DOMContentLoaded", function () {
+        
+        const winnerSelect = document.getElementById("winner_commander");
+        const loserSelect = document.getElementById("loser_commander");
+
+        // Store the original options for the loser selector
+        const originalLoserOptions = Array.from(loserSelect.options);
+
+        winnerSelect.addEventListener("change", function () {
+            const winnerId = winnerSelect.value;
+
+            // Clear the loser selector
+            loserSelect.innerHTML = "";
+
+            // Add back the original options except for the selected winner
+            originalLoserOptions.forEach(function (option) {
+
+                if (option.value !== winnerId) {
+
+                    loserSelect.appendChild(option);
+
+                }
+
+            });
+
+        });
+
+    });
+
+</script>
 
 @endsection
